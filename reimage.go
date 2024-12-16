dcafa33102bdb6822297240bcc788e21a88c16cf
Tristan Colgate-McFarlane <tristan@cerbos.dev>
2024-12-16 14:25:28 +0000

    chore(licensing): update license headers
    Signed-off-by: Tristan Colgate-McFarlane <tristan@cerbos.dev>

--- reimage.go
+++ reimage.go
@@ -1,4 +1,4 @@
-// Copyright 2021-2023 Zenauth Ltd.
+// Copyright 2021-2024 Zenauth Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Package reimage provides tools for processing/updating the images listed in k8s manifests
--- trivy.go
+++ trivy.go
@@ -1,3 +1,6 @@
+// Copyright 2021-2024 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
 package reimage
 
 import (
