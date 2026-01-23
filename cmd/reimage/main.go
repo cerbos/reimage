8e931fb21e41f8c098b80dc78cc2b256fe797214
Tristan Colgate-McFarlane <tristan@cerbos.dev>
2026-01-23 16:19:01 +0000

    fix copyright headers

--- cmd/reimage/main.go
+++ cmd/reimage/main.go
@@ -1,4 +1,4 @@
-// Copyright 2021-2025 Zenauth Ltd.
+// Copyright 2021-2026 Zenauth Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Package main is the main reimage binary
--- kms.go
+++ kms.go
@@ -1,4 +1,4 @@
-// Copyright 2021-2025 Zenauth Ltd.
+// Copyright 2021-2026 Zenauth Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Package reimage provides tools for processing/updating the images listed in k8s manifests
